11209c7d1dbfb68a1437864e6b08b0c9f7864d56
UCC_AMS_2310_Equipo03 <johan.perezt@campusucc.edu.co>
2023-05-09 14:02:30 -0500

    marvel-seriesv2.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
 
 function App() {
+  const [comics, setComics] = useState([]);
+  useEffect(() => {
+    axios.get('https://ol3vxlqnunbxuh4k2on7wahgom0lzohs.lambda-url.us-east-1.on.aws/')
+      .then(response => {
+        console.log(response.data.data.results)
+        setComics(response.data.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1>marvel</h1>
+      <h1>Marvel</h1>
+      <div id="page-content" className="page-content">
+        <Row>
+          {comics.map(comic => (
+            <Col md={3} key={comic.id}>
+              <Card>
+                <Card.Img variant="top" src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} />
+                <Card.Body>
+                  <Card.Title>{comic.title}</Card.Title>
+                  <Card.Text>{comic.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
     </div>
   );
 }
