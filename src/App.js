dee10f743a3ed47bc5c3e72daa608fa578539ec2
UCC_AMS_2310_Equipo03 <johan.perezt@campusucc.edu.co>
2023-05-09 21:12:31 -0500

    marvel-seriesv3.1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,92 @@
 import './App.css';
 import axios from 'axios'
 import { useState, useEffect } from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
-
+import marvelLogo from "./assets/marvel-logo.jpg";
+import logoUsuario from "./assets/logo-usuario.png";
+import marvelSub from "./assets/marvel-sub.png";
+import searchLogo from "./assets/search.png";
+ 
 function App() {
   const [comics, setComics] = useState([]);
   useEffect(() => {
     axios.get('https://ol3vxlqnunbxuh4k2on7wahgom0lzohs.lambda-url.us-east-1.on.aws/')
       .then(response => {
-        console.log(response.data.data.results)
         setComics(response.data.data.results);
       })
       .catch(error => {
         console.log(error);
       });
   }, []);
-
+ 
   return (
-    <div className="App">
-      <h1>Marvel</h1>
-      <div id="page-content" className="page-content">
-        <Row>
-          {comics.map(comic => (
-            <Col md={3} key={comic.id}>
-              <Card>
-                <Card.Img variant="top" src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} />
-                <Card.Body>
-                  <Card.Title>{comic.title}</Card.Title>
-                  <Card.Text>{comic.description}</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+    <div className="container-fluid px-0">
+      <div className="container-fluid px-0">
+        <div className="header">
+          <div id='elem'>
+            <img id='logousu' src={logoUsuario} alt="Logo Usuario" className="img-fluid" />
+            <h1 id='text1' className="d-inline-block ml-2 mb-0 text-white">JOHAN</h1>
+          </div>
+          <div id='elem' className='elem1'>
+            <img src={marvelLogo} alt="Marvel Logo" className="img-fluid" />
+          </div>
+          <div id='elem'>
+            <img src={marvelSub} alt="Marvel Unlimited Subscribe" className="img-fluid" />
+            <h1 id='text2' className="d-inline-block ml-10 mb-0 text-white">MARVEL UNLIMITED SUBSCRIBE</h1>
+          </div>
+          <div id='elem'>
+            <img id='searchl' src={searchLogo} alt="Search Logo" className="img-fluid" />
+          </div>
+        </div>
+
+        <nav className="navbar navbar-expand-md navbar-dark bg-marvel py-0">
+          <div className="container">
+            <div className="" id="navbarNav">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">CHARACTERS</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">COMICS</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">SERIES</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">STORIES</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <h1 className="text-center mt-5 mb-5">Marvel Series</h1>
+      <div className="row gx-2 gx-lg-3">
+        {comics.map((comic) => (
+          <div key={comic.id} className="col-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+            <div className="card h-100">
+              <img
+                className="card-img-top img-fluid"
+                src={`${comic.thumbnail.path}/portrait_incredible.${comic.thumbnail.extension}`}
+                alt={comic.title}
+              />
+              <div className="card-body p-1">
+                <h6 className="card-title fw-bold">{comic.title}</h6>
+                <p className="card-text mb-1">
+                  <strong>Titulo: </strong>
+                  {comic.title}
+                </p>
+                <p className="card-text mb-1">
+                  <strong>Descripcion: </strong>
+                  {comic.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  );
+  );  
 }
-
+ 
 export default App;
